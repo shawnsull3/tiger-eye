307fd701ff2d233f954975bc86c80eef73a0a9bd
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-13 07:56:29 -0600

    Add link to from Product Overview to reviews

--- src/Components/Reviews/Ratings.jsx
+++ src/Components/Reviews/Ratings.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable */
 import React from 'react';
 
 const Ratings = ({ reviews }) => (
   <div>
+    <a id="reviews" />
     <div className="border col-12 col-sm-12 col-lg-12 col-xl-12">
       RATINGS
     </div>
