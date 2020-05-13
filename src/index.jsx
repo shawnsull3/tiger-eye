6633f88cc611b0aa354e3db44b86e6670344bf50
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-13 15:42:06 -0600

    General code cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -21,7 +21,6 @@ const reducer = combineReducers({
 });
 
 const middleware = [thunk];
-/* eslint-disable no-underscore-dangle */
 const store = createStore(reducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
@@ -33,7 +32,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
